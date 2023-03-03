e01f08c892e19322ccd931c2ffe1ca67e9997972
Daniel Correa <yo@danielgara.com>
2023-03-03 08:43:42 -0500

    first commit

---
+++ app/Http/Controllers/ProductController-type-play.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class ProductController extends Controller
+{
+    public function index(): View
+    {
+        $viewData = [];
+        $viewData['title'] = 'Products - Online Store';
+        $viewData['subtitle'] = 'List of products';
+        $viewData['products'] = Product::all();
+
+        return view('product.index')->with('viewData', $viewData);
+    }
+
+    public function show(string $id): View
+    {
+        $comment = Comment::findOrFail(1);
+        $comment->setId('8');
+        dd($comment->getId());
+        $viewData = [];
+        $product = Product::findOrFail($id);
+        $viewData['title'] = $product['name'].' - Online Store';
+        $viewData['subtitle'] = $product['name'].' - Product information';
+        $viewData['product'] = $product;
+
+        return view('product.show')->with('viewData', $viewData);
+    }
+
+    public function create(): View
+    {
+        $viewData = []; //to be sent to the view
+        $viewData['title'] = 'Create product';
+
+        return view('product.create')->with('viewData', $viewData);
+    }
+
+    public function save(Request $request): \Illuminate\Http\RedirectResponse
+    {
+        $request->validate([
+            'name' => 'required',
+            'price' => 'required',
+        ]);
+
+        Product::create($request->only(['name', 'price']));
+
+        return back();
+    }
+}
